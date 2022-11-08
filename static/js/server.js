641b63a3a4a11c0ea4287070516d2962a709cea1
GH <sukpo6010@gmail.com>
2022-11-08 14:26:32 +0900

    first

---
+++ static/js/server.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+            show_comment()
+        });
+
+
+        function save_comment() {
+
+            let name = $('#name').val()
+            let comment = $('#comment').val()
+            let person = "go"
+
+            $.ajax({
+                type: 'POST',
+                url: '/comment',
+                data: {'name_give' : name, 'comment_give' : comment, 'person_give' : person},
+                success: function (response) {
+                    alert(response['msg'])
+                    window.location.reload()
+                }
+            })
+        }
+
+        function show_comment() {
+            $.ajax({
+                type: "GET",
+                url: "/comment",
+                data: {},
+                success: function (response) {
+
+                    let rows = response['comments']
+
+                    for (i = 0; i < rows.length; i++) {
+
+                        let person = rows[i]['person']
+                        let name = rows[i]['name']
+                        let comment = rows[i]['comment']
+
+                        let temp_html = `<div class="card">
+                                            <div class="card-body">
+                                                <blockquote class="blockquote mb-0">
+                                                    <p>${comment}</p>
+                                                    <footer class="blockquote-footer">${name}</footer>
+                                                </blockquote>
+                                            </div>
+                                        </div>`
+
+
+                            $("#comment-list").append(temp_html)
+
+                            $("#comment-list").append()
+
+
+
+                    }
+                }
+            });
+        }
